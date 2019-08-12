a034ac5aba98afc3adbf93df84174a7998b48fdc
Madz96 <mad.subasinghe@gmail.com>
2019-08-12 08:49:07 +0530

    Added main & main2 components to library

--- projects/angt-lib/src/lib/angt-lib.module.ts
+++ projects/angt-lib/src/lib/angt-lib.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AngtLibComponent } from './angt-lib.component';
+import { MainComponent } from './main/main.component';
+import { Main2Component } from './main2/main2.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [AngtLibComponent],
-  exports: [AngtLibComponent]
+  declarations: [AngtLibComponent, MainComponent, Main2Component],
+  exports: [AngtLibComponent, MainComponent, Main2Component]
 })
 export class AngtLibModule { }
---
+++ projects/angt-lib/src/lib/main2/main2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'angtl-main2',
+  templateUrl: './main2.component.html',
+  styleUrls: ['./main2.component.css']
+})
+export class Main2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
